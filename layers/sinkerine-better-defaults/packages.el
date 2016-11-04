47ca3c48e6f6e180985c7a596a199de519004596
Sinkerine <git@15cm.net>
2016-11-04 12:09:27 +0800

    [-] Remove redundant commemt

--- layers/sinkerine-better-defaults/packages.el
+++ layers/sinkerine-better-defaults/packages.el
@@ -9,26 +9,6 @@
 ;;
 ;;; License: GPLv3
 
-;;; Commentary:
-
-;; See the Spacemacs documentation and FAQs for instructions on how to implement
-;; a new layer:
-;;
-;;   SPC h SPC layers RET
-;;
-;;
-;; Briefly, each package to be installed or configured by this layer should be
-;; added to `sinkerine-better-defaults-packages'. Then, for each package PACKAGE:
-;;
-;; - If PACKAGE is not referenced by any other Spacemacs layer, define a
-;;   function `sinkerine-better-defaults/init-PACKAGE' to load and initialize the package.
-
-;; - Otherwise, PACKAGE is already referenced by another Spacemacs layer, so
-;;   define the functions `sinkerine-better-defaults/pre-init-PACKAGE' and/or
-;;   `sinkerine-better-defaults/post-init-PACKAGE' to customize the package as it is loaded.
-
-;;; Code:
-
-(defconst sinkerine-better-defaults-packages
+defconst sinkerine-better-defaults-packages
   '()
   )
--- layers/sinkerine-writing/packages.el
+++ layers/sinkerine-writing/packages.el
@@ -9,26 +9,6 @@
 ;;
 ;;; License: GPLv3
 
-;;; Commentary:
-
-;; See the Spacemacs documentation and FAQs for instructions on how to implement
-;; a new layer:
-;;
-;;   SPC h SPC layers RET
-;;
-;;
-;; Briefly, each package to be installed or configured by this layer should be
-;; added to `sinkerine-writting-packages'. Then, for each package PACKAGE:
-;;
-;; - If PACKAGE is not referenced by any other Spacemacs layer, define a
-;;   function `sinkerine-writting/init-PACKAGE' to load and initialize the package.
-
-;; - Otherwise, PACKAGE is already referenced by another Spacemacs layer, so
-;;   define the functions `sinkerine-writing/pre-init-PACKAGE' and/or
-;;   `sinkerine-writing/post-init-PACKAGE' to customize the package as it is loaded.
-
-;;; Code:
-
 (defconst sinkerine-writing-packages
   '(
     org

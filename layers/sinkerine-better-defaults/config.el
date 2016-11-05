57cb75a2f028810ac10b9de006c270180c6ae2b1
Sinkerine <git@15cm.net>
2016-11-05 17:52:52 +0800

    [+] Set utf-8 encoding

--- layers/sinkerine-better-defaults/config.el
+++ layers/sinkerine-better-defaults/config.el
@@ -1,9 +1,9 @@
-;; (setq utf-translate-cjk-mode nil) ; disable CJK coding/encoding (Chinese/Japanese/Korean characters)
-;; (set-language-environment 'utf-8)
-;; ;; (set-keyboard-coding-system 'utf-8-mac) ; For old Carbon emacs on OS X only
-;; (setq locale-coding-system 'utf-8)
-;; (set-default-coding-systems 'utf-8)
-;; (set-terminal-coding-system 'utf-8)
-;; (set-selection-coding-system 'utf-8)
-;; (prefer-coding-system 'utf-8)
+(setq utf-translate-cjk-mode nil) ; disable CJK coding/encoding (Chinese/Japanese/Korean characters)
+(set-language-environment 'utf-8)
+;; (set-keyboard-coding-system 'utf-8-mac) ; For old Carbon emacs on OS X only
+(setq locale-coding-system 'utf-8)
+(set-default-coding-systems 'utf-8)
+(set-terminal-coding-system 'utf-8)
+(set-selection-coding-system 'utf-8)
+(prefer-coding-system 'utf-8)
 (spacemacs//set-monospaced-font "M+ 1m" "Hiragino Sans GB W3" 12 12)

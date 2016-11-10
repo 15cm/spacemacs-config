0d67d7d78b1fb1aedf038a0a0994bd0492e16422
Sinkerine <git@15cm.net>
2016-11-10 15:14:20 +0800

    Modified

--- init.el
+++ init.el
@@ -67,9 +67,7 @@ values."
    ;; wrapped in a layer. If you need some configuration for these
    ;; packages, then consider creating a layer. You can also put the
    ;; configuration in `dotspacemacs/user-config'.
-   dotspacemacs-additional-packages '(
-                                      ;; impatient-mode
-                                      )
+   dotspacemacs-additional-packages '()
    ;; A list of packages and/or extensions that will not be install and loaded.
    dotspacemacs-excluded-packages '()
    ;; If non-nil spacemacs will delete any orphan packages, i.e. packages that

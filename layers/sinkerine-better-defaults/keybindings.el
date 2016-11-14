6f87b4e292a062dc27c465814ce9cafa57ad9542
Sinkerine <git@15cm.net>
2016-11-14 14:01:40 +0800

    [+] Prefix 'o' for better-defaults [+] bindings for my-find-notes [+] bindings for fzf

--- layers/sinkerine-better-defaults/keybindings.el
+++ layers/sinkerine-better-defaults/keybindings.el
@@ -1,3 +1,8 @@
 ;; (evil-leader/set-key "C-c" 'switch-clipboard-mode)
 (evil-leader/set-key "C-r" 'dotspacemacs/sync-configuration-layers)
 (evil-leader/set-key "C-d" 'spacemacs/find-dotfile)
+
+(spacemacs/declare-prefix "o" "better-defaults")
+(evil-leader/set-key "on" 'my-find-notes)
+(evil-leader/set-key "of" 'fzf-directory)
+

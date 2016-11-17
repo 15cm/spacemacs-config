d223f8e85ff4135aec06b10eee15eef74212016c
Sinkerine <git@15cm.net>
2016-11-17 21:52:41 +0800

    [!] modify personal keybindings

--- layers/sinkerine-better-defaults/keybindings.el
+++ layers/sinkerine-better-defaults/keybindings.el
@@ -2,7 +2,8 @@
 (evil-leader/set-key "C-r" 'dotspacemacs/sync-configuration-layers)
 (evil-leader/set-key "C-d" 'spacemacs/find-dotfile)
 
-(spacemacs/declare-prefix "o" "better-defaults")
-(evil-leader/set-key "on" 'my-find-notes)
-(evil-leader/set-key "of" 'fzf-directory)
+(spacemacs/declare-prefix "<SPC>" "better-defaults")
+(evil-leader/set-key "<SPC>n" 'my-find-notes)
+(evil-leader/set-key "<SPC>f" 'fzf)
+(evil-leader/set-key "<SPC>d" 'fzf-directory)
 

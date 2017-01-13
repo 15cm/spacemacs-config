844dd47df9c77bd29d469116b6ebc28c3500b091
Sinkerine <git@15cm.net>
2017-01-13 20:03:39 +0800

    [!] Change function name

--- layers/sinkerine-better-defaults/funcs.el
+++ layers/sinkerine-better-defaults/funcs.el
@@ -29,12 +29,34 @@
 ;;            ))
 ;;   )
 
-(defun my-find-notes ()
+(defun my-fzf-notes ()
   (interactive)
   (fzf-directory "~/Geek/Wiki/notes/")
   )
 
-(defun my-find-codes ()
+(defun my-fzf-codes ()
   (interactive)
   (fzf-directory "~/Geek/Wiki/static/code/")
+  )
+
+(defun my-find-wiki ()
+  (interactive)
+  (helm-find-files-1 (concat user-home-directory "Geek/Wiki/"))
+  )
+
+(defun my-iterm-goto-filedir-or-home ()
+  "Go to present working dir and focus iterm"
+  (interactive)
+  (do-applescript
+   (concat
+    " tell application \"iTerm2\"\n"
+    "   tell the current session of current window\n"
+    (format "     write text \"cd %s\" \n"
+            ;; string escaping madness for applescript
+            (replace-regexp-in-string "\\\\" "\\\\\\\\"
+                                      (shell-quote-argument (or default-directory "~"))))
+    "   end tell\n"
+    " end tell\n"
+    " do shell script \"open -a iTerm\"\n"
+    ))
   )
--- layers/sinkerine-better-defaults/keybindings.el
+++ layers/sinkerine-better-defaults/keybindings.el
@@ -3,10 +3,12 @@
 (evil-leader/set-key "C-d" 'spacemacs/find-dotfile)
 
 (spacemacs/declare-prefix "<SPC>" "better-defaults")
-(evil-leader/set-key "<SPC>n" 'my-find-notes)
-(evil-leader/set-key "<SPC>c" 'my-find-codes)
+(evil-leader/set-key "<SPC>n" 'my-fzf-notes)
+(evil-leader/set-key "<SPC>c" 'my-fzf-codes)
 (evil-leader/set-key "<SPC>f" 'fzf)
 (evil-leader/set-key "<SPC>d" 'fzf-directory)
+(evil-leader/set-key "<SPC>t" 'my-iterm-goto-filedir-or-home)
+(evil-leader/set-key "<SPC>w" 'my-find-wiki)
 
 (define-key company-active-map (kbd "C-f") 'right-char)
 (define-key company-filter-map (kbd "C-f") 'right-char)

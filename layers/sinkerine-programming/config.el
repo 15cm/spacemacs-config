2dbfee0d5a71b9d05af98737a6f0adac21d3f27d
Sinkerine <git@15cm.net>
2017-05-05 17:15:56 +0800

    [!] Fix company-c-header config

--- layers/sinkerine-programming/config.el
+++ layers/sinkerine-programming/config.el
@@ -4,9 +4,13 @@
 (setq ycmd-force-semantic-completion t)
 
 ;; company c headers
+(setq c-c++-enable-clang-support nil)
 (setq company-c-headers-path-system '("/usr/include/c++/4.2.1/"))
 
 ;; default indent style
 (setq-default indent-tabs-mode nil)
 (setq-default tab-width 4)
 (setq-default tab-always-indent nil)
+
+;; emmet-mode
+(setq emmet-move-cursor-after-expanding nil) ;; default t

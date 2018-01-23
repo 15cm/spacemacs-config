0cbe3fbddbac9633d46cd1b98078a9cd0312527f
Sinkerine <git@15cm.net>
2018-01-23 00:38:19 -0500

    [+] kbd for dumb-jump

--- layers/my-programming/keybindings.el
+++ layers/my-programming/keybindings.el
@@ -4,3 +4,9 @@
 (spacemacs/set-leader-keys-for-major-mode 'js2-mode "nb" 'nodejs-repl-send-buffer)
 (spacemacs/set-leader-keys-for-major-mode 'js2-mode "ne" 'nodejs-repl-send-last-sexp)
 (spacemacs/set-leader-keys-for-major-mode 'js2-mode "ni" 'run-node)
+
+(evil-leader/set-key "jgd" 'dumb-jump-go)
+(evil-leader/set-key "jgD" 'dumb-jump-go-other-window)
+(evil-leader/set-key "jgb" 'dumb-jump-back)
+(evil-leader/set-key "jgp" 'dumb-jump-go-prompt)
+(evil-leader/set-key "jgq" 'dumb-jump-quick-look)

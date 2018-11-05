d7fe1bb153e45e6f9f8bd84ff0a2d2e69e4b8b7b
Sinkerine <git@15cm.net>
2018-11-04 20:04:10 -0500

    rm(org-brain): disable org-brain, remove related config

--- layers/my-writing/packages.el
+++ layers/my-writing/packages.el
@@ -89,8 +89,8 @@
 
 (defun my-writing/post-init-org-brain()
   (with-eval-after-load 'org-brain
-    (setq org-brain-path "~/resilio-sync/personal/brain")
-    (setq org-id-locations-file "~/resilio-sync/personal/brain/.org-id-locations")
+    ;; (setq org-brain-path "~/resilio-sync/personal/brain")
+    ;; (setq org-id-locations-file "~/resilio-sync/personal/brain/.org-id-locations")
     (with-eval-after-load 'evil
       (evil-set-initial-state 'org-brain-visualize-mode 'normal)
       (evil-make-overriding-map org-brain-visualize-mode-map 'normal)))

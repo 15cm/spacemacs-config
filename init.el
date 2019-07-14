a41ce02af9f4d4bfaf33acda8e0048c82bfb2296
Sinkerine <github@15cm.net>
2019-07-13 21:15:27 -0700

    Minor changes

--- init.el
+++ init.el
@@ -498,7 +498,7 @@ values."
    ;; A value from the range (0..100), in increasing opacity, which describes
    ;; the transparency level of a frame when it's active or selected.
    ;; Transparency can be toggled through `toggle-transparency'. (default 90)
-   dotspacemacs-active-transparency 95
+   dotspacemacs-active-transparency 90
 
    ;; A value from the range (0..100), in increasing opacity, which describes
    ;; the transparency level of a frame when it's inactive or deselected.
